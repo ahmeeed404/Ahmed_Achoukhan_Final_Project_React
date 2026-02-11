1c3390b13bc0b497362b9267ab628b57ed8a3a62
ahmeeed404 <ahmed.achoukhan04@gmail.com>
2026-02-11 12:04:00 +0100

    commit import of all images

---
+++ src/constant/images.jsx
@@ -0,0 +1,91 @@
+import banner02 from '../assets/images/banner-02.jpg';
+import banner7 from '../assets/images/banner-07.jpg';
+import banner8 from '../assets/images/banner-08.jpg';
+import banner from '../assets/images/banner.webp';
+import bgVideo from '../assets/images/bg_video_01.jpg.webp';
+import card1 from '../assets/images/cards1.webp';
+import card2 from '../assets/images/cards2.webp';
+import card3 from '../assets/images/cards3.webp';
+import card4 from '../assets/images/cards4.webp';
+import card5 from '../assets/images/cards5.webp';
+import card6 from '../assets/images/cards6.webp';
+import blog1 from '../assets/images/blog-01.jpg';
+import blog2 from '../assets/images/blog-02.jpg';
+import blog3 from '../assets/images/blog-03.jpg';
+import carousel1 from '../assets/images/carousel1.jpg';
+import carousel2 from '../assets/images/carousel2.jpg';
+import carousel3 from '../assets/images/carousel3.webp';
+import discover from '../assets/images/discover.jpg.webp';
+import express from '../assets/images/express.jpg.webp';
+import gallery3 from '../assets/images/gallery-03.jpg';
+import gallery7 from '../assets/images/gallery-07.jpg';
+import gallery9 from '../assets/images/gallery-09.jpg';
+import gallery13 from '../assets/images/gallery-13.jpg';
+import gallery15 from '../assets/images/gallery-15.jpg';
+import image from '../assets/images/image.png';
+import item2 from  '../assets/images/item-02.jpg';
+import item3 from  '../assets/images/item-03.jpg';
+import item5 from  '../assets/images/item-05.jpg';
+import item7 from  '../assets/images/item-07.jpg';
+import itemCart1 from '../assets/images/item-cart-01.jpg';
+import itemCart2 from '../assets/images/item-cart-02.jpg';
+import itemCart3 from '../assets/images/item-cart-03.jpg';
+import logo from '../assets/images/logo.png.webp';
+import masterCard from '../assets/images/mastercard.png.webp';
+import paypal from '../assets/images/paypal.png.webp';
+import shop from '../assets/images/shop-item-09.jpg';
+import shopi2 from '../assets/images/shop-item-09.webp';
+import shopy3 from '../assets/images/shop-item-09.webp';
+import shopy4 from '../assets/images/shop-item-09.webp';
+import shopy5 from '../assets/images/shop-item-09.webp';
+import shopy6 from '../assets/images/shop-item-09.webp';
+import shopy7 from '../assets/images/shop-item-09.webp';
+import shopy8 from '../assets/images/shop-item-09.webp';
+import visa from '../assets/images/visa.jpg.webp';
+
+export const images = {
+  banner02,
+  banner7,
+  banner8,
+  banner,
+  bgVideo,
+  card1,
+  card2,
+  card3,
+  card4,
+  card5,
+  card6,
+  blog1,
+  blog2,
+  blog3,
+  carousel1,
+  carousel2,
+  carousel3,
+  discover,
+  express,
+  gallery3,
+  gallery7,
+  gallery9,
+  gallery13,
+  gallery15,
+  image,
+  item2,
+  item3,
+  item5,
+  item7,
+  itemCart1,
+  itemCart2,
+  itemCart3,
+  logo,
+  masterCard,
+  paypal,
+  shop,
+  shopi2,
+  shopy3,
+  shopy4,
+  shopy5,
+  shopy6,
+  shopy7,
+  shopy8,
+  visa,
+};
